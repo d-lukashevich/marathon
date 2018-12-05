e0a3c8594b01bcba6471ef7c84f757930bd891c1
Лукашевич Денис Анатольевич <dlukashevich@synergy.ru>
2018-12-05 18:58:13 +0300

    WIP: added categories n some data

--- js/script.js
+++ js/script.js
@@ -30,19 +30,59 @@ new Vue({
             monday: {
                 name: 'Понедельник',
                 eatings: {
-                    breakfast: 'Овсянка',
-                    bite1: 'Грецкие орехи',
-                    lunch: 'Соба, макароны'
+                    breakfast: 'Овсяная каша (5 ст. л.) + курага/изюм',
+                    bite1: 'Грецкие орехи (30 гр) и сухофрукты (20гр)',
+                    lunch: 'Соба/акароны из твёрдых сортов + Адыгейский сыр + Салат',
+                    bite2: 'Обезжиренный творог + Печёная груша (1/2)',
+                    dinner: 'Салат с брынзой/тофу',
+                    lateDinner: 'Протеиновый коктейль / Кефир / Йогурт'
                 }
             },
             tuesday: {
                 name: 'Вторник',
                 eatings: {
-                    breakfast: 'Пшенка',
-                    bite1: 'Урбеч',
-                    lunch: 'Спаржа'
+                    breakfast: 'Пшеная каша (5 ст. л.) + груша (1/2) и мёд',
+                    bite1: 'Урбеч с мёдом (20 + 20гр)',
+                    lunch: 'Соевая спаржа + тушенные овощи + Салат',
+                    bite2: 'Протеиновый коктейль / Кефир',
+                    dinner: 'Овощные котлетки с адыг. сыром + запечённые кабачки + салат',
+                    lateDinner: 'Молоко с корицей'
+                }
+            },
+            wednesday: {
+                name: 'Среда',
+                eatings: {
+                    breakfast: 'Фркутовый салат',
+                    bite1: 'Бутерброд с сыром и зеленью',
+                    lunch: 'Овощной суп + лепешка/хлебец + салат',
+                    bite2: 'Урбеч с мёдом (20 + 20гр)',
+                    dinner: 'Обезжиренный йогурт + овощи на пару',
+                    lateDinner: 'Молоко с корицей'
+                }
+            },
+            thursday: {
+                name: 'Четверг',
+                eatings: {
+                    breakfast: 'Ячневая/перловая крупа (4 ст. л.) + яблоко/банан(1/2)',
+                    bite1: 'Протеиновый коктейль/орехи',
+                    lunch: 'Нут с подливкой + Бурый рис + ист салата',
+                    bite2: 'Салат / Авокадо',
+                    dinner: 'Сабджи + Салат',
+                    lateDinner: 'Йогурт/Кефир'
                 }
             }
         }
     }
 });
+
+function createScreen(e) {
+    e.preventDefault();
+    html2canvas(this.parentElement.querySelector('table')).then(canvas => {
+        let screenshot = canvas.toDataURL("image/png").replace(/^data:image\/png/, "data:application/octet-stream");
+        $(this).attr('download', 'schedule.png').attr('href', screenshot);
+        $(this).off( "click", createScreen);
+        $(this)[0].click();
+    });
+}
+
+$(".schedule__btn").on('click', createScreen);
